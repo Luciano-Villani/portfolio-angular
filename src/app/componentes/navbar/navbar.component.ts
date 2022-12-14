65a970d1edff21fd6e5bb9e8da2959476ad8748d
luciano-villani <lucianov@visualdigital.com.ar>
2022-12-13 21:15:13 -0300

    hotfix navbar

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -14,7 +14,7 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
     this.datosPorfoio.obtenerDatos().subscribe(data=>{
       console.log(data);
-      this.misRedes=data.redesSociales;
+      this.misRedes=data.RedesSociales;
 
     });
   
